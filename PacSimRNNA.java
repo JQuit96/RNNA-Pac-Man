1ac66b84a1e4cbb40327ac0c9842b0319d9d4197
JQuit96 <julian.quitian@gmail.com>
2017-10-01 22:03:24 -0400

    Assignment files and Simple Replan added

---
+++ PacSimRNNA.java
@@ -0,0 +1,42 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import pacsim.BFSPath;
+import pacsim.PacAction;
+import pacsim.PacCell;
+import pacsim.PacFace;
+import pacsim.PacSim;
+import pacsim.PacUtils;
+import pacsim.PacmanCell;
+
+/**
+ * RNNA
+ * @author Julian Quitian, Ley Widley
+ */
+public class PacSimRNNA implements PacAction {
+   
+   private List<Point> path;
+   private int simTime;
+      
+   public PacSimRNNA( String fname ) {
+      PacSim sim = new PacSim( fname );
+      sim.init(this);
+   }
+   
+   public static void main( String[] args ) {
+      System.out.println("\nTSP using RNNA agent by Julian Quitian & Ley Widley:");
+      System.out.println("\nMaze : " + args[ 0 ] + "\n" );
+      new PacSimRNNA( args[ 0 ] );
+   }
+
+   @Override
+   public void init() {
+      simTime = 0;
+      path = new ArrayList();	
+   }
+   
+   @Override
+   public PacFace action( Object state ) {
+	
+   }
+}
