fde69920eb5c9ae353e4a33ae624e3bec7fed5bc
widley95 <widley95@gmail.com>
2017-10-07 04:12:16 -0400

    Updated .gitignore file. Added food array in PacSimRNNA. - Bugged

--- PacSimRNNA.java
+++ PacSimRNNA.java
@@ -27,7 +27,7 @@ public class PacSimRNNA implements PacAction {
 	public static void main( String[] args ) {
 		System.out.println("\nTSP using RNNA agent by Julian Quitian & Ley Widley:");
 		System.out.println("\nMaze : " + args[ 0 ] + "\n" );
-		new PacSimRNNA( args[ 0 ] );
+		//new PacSimRNNA( args[ 0 ] );
 	}
 
 	// Use this method to reset any variables that must be re-initialized before runs
@@ -54,14 +54,48 @@ public class PacSimRNNA implements PacAction {
 		PacmanCell pc = PacUtils.findPacman(grid);
 
 		// Make sure Pac-Man is in the game
-		if (pc == null) return null;
+		if (pc == null) 
+			return null;
 
 		// Step 1: Compute RNNA solution path only when method is first called
 		if(path.isEmpty()){
-			path = new ArrayList();
+			// Generate the Food array
+			List<Point> allFoodPellets = generateFoodArray(grid);
+			int population = 0, cost = 0; 
+			// Calculate each step in the algorithm 
+			for(int i = 0; i < allFoodPellets.size(); i++)
+			{
+				System.out.println("Population at step " + i + ":");
+				population = 0; 
+				cost = 0;
+				for (int j = 0; j < allFoodPellets.size(); j++)
+				{
+					Point currentFood = allFoodPellets.get(j);
+					cost = PacUtils.manhattanDistance(pc.loc, currentFood);
+					System.out.println(population + " : cost="+ cost + " : [( "+
+					currentFood.getX() + "," + currentFood.getY() + ")," +
+							cost + "]");
+					population++;
+				}
+			}
+
 			//TODO compute solution path
 		}
 
 		// TODO Note current position and next step; return NSEW direction in form of PacFace enum
+		
    }
+	// Given the grid, get all the food location 
+	// from the grid, and then print them out. 
+	public List<Point> generateFoodArray(PacCell[][] G)
+	{
+		List<Point> allFood = PacUtils.findFood(G); 
+		System.out.println("Food Array\n");
+		for (int i = 0; i < allFood.size(); i++)
+		{
+			System.out.println(i + " : (" + allFood.get(i).getX() + "," + allFood.get(i).getY() + ")");
+		}
+		
+		return allFood; 
+	}
 }
